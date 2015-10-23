328df3fd67a033cb5e0129c7b301934c62f52a67
majorjoel2 <joel@tanner-family.org>
2015-10-22 21:53:21 -0400

    It is all on one line.

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -2,40 +2,65 @@ import java.util.Scanner;
 
 public class PigLatin {
 	
-	public String getInput() {	//Greg
+	public static String[] getInput() {	//Greg
 		Scanner keybrd = new Scanner(System.in);
 		System.out.print("Text to be converted: ");
 		String pigSentence = keybrd.nextLine();
-		return pigSentence;
+		return pigSentence.toLowerCase().split(" ");
 	}
 	
-	public boolean firstVovel(String pigSentence) {	//Grant
+	public static boolean firstVovel(String pigSentence) {	//Grant
 		String firstLetter = pigSentence.substring(0, 1);
-		if(firstLetter == "a" || firstLetter == "e" ||
-				firstLetter ==  "i" || firstLetter == "o" || firstLetter == "u") {
+		if(firstLetter.equals("a") || firstLetter.equals("e") ||
+				firstLetter.equals("i") || firstLetter.equals("o") || firstLetter.equals("u")) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	public String addYay(String pigSentence) {	//Joel Tanner 2015.10.15
+	public static String addYay(String pigSentence) {	//Joel Tanner 2015.10.15
 		pigSentence = pigSentence + "yay";
 		return pigSentence;
 	}
 	
+	public static String moveToEnd(String pigSentence) {	//David
+		String firstLetter = "";
+		firstLetter = pigSentence.substring(0, 1);
+		String otherPart = "";
+		otherPart = pigSentence.substring(1, pigSentence.length());
+		pigSentence = otherPart + firstLetter;
+		return pigSentence;
+	}
 	
-	
-	public String addAy(String pigSentence) { //Jimin
+	public static String addAy(String pigSentence) { //Jimin
 		pigSentence = pigSentence + "ay";
 		return pigSentence;
 	}
 	
-	public void printYo(String pigSentence) { //Haley
+	public static void printYo(String pigSentence) { //Haley
 		System.out.println(pigSentence);
 	}
 	
 	public static void main(String[] args) {
-		
+		String[] pigSentence = getInput();
+		String pigWord = "";
+		String pigFinal = "";
+		for(int i = 0; i < pigSentence.length; i++) {
+			pigWord = pigSentence[i];
+			if(firstVovel(pigWord)) {
+				pigWord = addYay(pigWord);
+			} else {
+				pigWord = moveToEnd(pigWord);
+				pigWord = addAy(pigWord);
+			}
+			if(i == (pigSentence.length)-1) {
+				pigWord = pigWord + "";
+			} else {
+				pigWord = pigWord + " ";
+			}
+			pigFinal = pigFinal + pigWord;
+		}
+		printYo(pigFinal);
 	}
 }
